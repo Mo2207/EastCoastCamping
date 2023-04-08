82b0dd63ca3f817f1438905cc79ac6d1d7ff50f4
Aung Han <aunghan@Aungs-MacBook-Pro.local>
2023-04-08 08:54:28 -0300

    added intro section

--- east-coast-camping/client/src/pages/Home.js
+++ east-coast-camping/client/src/pages/Home.js
@@ -3,6 +3,7 @@ import Card from 'react-bootstrap/Card'; //react-bootstrap
 import CardGroup from 'react-bootstrap/CardGroup'; //react-bootstrap
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
+import '../styles/Home.css'
 
 
 
@@ -10,10 +11,25 @@ import Container from 'react-bootstrap/Container';
 
 
 export default function Home() {
+
     return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
+        <>
+            <Card className='searchBar-Bg'>
+                <Card.Body>
+                    {/* <p>searchBar</p> */}
+                </Card.Body>
+            </Card>
+            <Container className='mt-5 text-center'>
+                <h2 className='mb-4'>Welcome to EastCoastCamping</h2>
+
+                <p>Looking for a memorable camping experience? Look no further than EastCoastCamping, where we connect you with the best campsites and outdoor destinations. </p>
+                <p>  Whether you're a seasoned outdoor enthusiast or a first-time camper, our website makes it easy to plan and book your perfect camping trip. </p>
+                <p> Our user-friendly platform makes it easy to find and reserve the perfect campsite for your next outdoor adventure.
+                </p>
+            </Container>
+
+
+        </>
 
 
 
