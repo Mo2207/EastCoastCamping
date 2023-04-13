d7fd9d305e43a1250ce40286c17ccd80e41742e0
Yelzaw <110345251+Yelzaw@users.noreply.github.com>
2023-04-13 19:07:03 -0300

    Added code for sign-out

--- east-coast-camping/client/src/components/Navbar.js
+++ east-coast-camping/client/src/components/Navbar.js
@@ -34,7 +34,7 @@ function NavTab() {
                             <>
                                 <Nav.Link href="/MyBookings">My Bookings</Nav.Link>
                                 <Nav.Link href="/me">Profile</Nav.Link>
-                                <Nav.Link href="/Login" >Logout</Nav.Link>
+                                <Nav.Link href="/Login" onClick={() => Auth.logout()}>Logout</Nav.Link>
                             </>
                         ):(
                             <>
