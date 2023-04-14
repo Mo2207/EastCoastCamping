a973dc99dc4742036cc48a6ebcc3d9a35348aa8d
Aung Han <doubleaung5@gmail.com>
2023-04-14 19:32:26 -0300

    fixed err

--- east-coast-camping/client/src/pages/Profile.js
+++ east-coast-camping/client/src/pages/Profile.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import { Navigate, useParams, useResolvedPath } from 'react-router-dom';
 import { useMutation, useQuery } from '@apollo/client';
-import Footer from '../components/Footer';
+
 import {
   MDBCol,
   MDBContainer,
@@ -18,6 +18,7 @@ import { Button } from 'react-bootstrap';
 import Auth from '../utils/auth';
 import { QUERY_ME } from '../utils/queries';
 import { DELETE_ME } from '../utils/mutations';
+import { EDIT_ME } from '../utils/mutations';
 // import NoMatch from './NoMatch';
 
 
@@ -43,6 +44,39 @@ export default function Profile() {
     console.log(data)
     localStorage.removeItem('id_token');
     window.location.assign('/regret');
+  }
+
+  const [editMe] = useMutation(EDIT_ME);
+
+  // edit mode when user pressed edit button
+  const [editMode, setEditMode] = useState(false);
+  // state variables for user profile
+  const [firstNameState, setFirstName] = useState('');
+  const [lastNameState, setLastName] = useState('');
+  const [emailState, setEmail] = useState('');
+
+  // everytime profile changes useEffect is called and sets the states
+  useEffect(() => {
+    if (profile.firstName) setFirstName(profile.firstName);
+    if (profile.lastName) setLastName(profile.lastName);
+    if (profile.email) setEmail(profile.email);
+  }, [profile])
+
+  // edit the users fields
+  function handleToEdit() {
+    // get the users id to hand into editMe
+    const userId = Auth.getToken();
+    // console.log(`userId is: ${userId}`)
+
+    setEditMode(false);
+    editMe({
+      variables: {
+        userId: userId,
+        firstName: firstNameState,
+        lastName: lastNameState,
+        email: emailState
+      }
+    })
   }
 
   return (
@@ -79,7 +113,12 @@ export default function Profile() {
                         <MDBCardText>First Name</MDBCardText>
                       </MDBCol>
                       <MDBCol sm="9">
-                        <MDBCardText className="text-muted">{user.firstName}</MDBCardText>
+                        {editMode ? (
+                          <input type="text" id='firstName' value={firstNameState} onChange={(e) => setFirstName(e.target.value)}>
+                          </input>
+                        ) : (
+                          <MDBCardText className="text-muted">{firstNameState}</MDBCardText>
+                        )}
                       </MDBCol>
                     </MDBRow>
                     <hr />
@@ -88,7 +127,12 @@ export default function Profile() {
                         <MDBCardText>Last Name</MDBCardText>
                       </MDBCol>
                       <MDBCol sm="9">
-                        <MDBCardText className="text-muted">{profile.lastName}</MDBCardText>
+                        {editMode ? (
+                          <input type="text" id='lastName' value={lastNameState} onChange={(e) => setLastName(e.target.value)}>
+                          </input>
+                        ) : (
+                          <MDBCardText className="text-muted">{lastNameState}</MDBCardText>
+                        )}
                       </MDBCol>
                     </MDBRow>
                     <hr />
@@ -97,17 +141,22 @@ export default function Profile() {
                         <MDBCardText>Email</MDBCardText>
                       </MDBCol>
                       <MDBCol sm="9">
-                        <MDBCardText className="text-muted">{profile.email}</MDBCardText>
+                        {editMode ? (
+                          <input type="text" id='email' value={emailState} onChange={(e) => setEmail(e.target.value)}>
+                          </input>
+                        ) : (
+                          <MDBCardText className="text-muted">{emailState}</MDBCardText>
+                        )}
                       </MDBCol>
                     </MDBRow>
                   </MDBCardBody>
                 </MDBCard>
                 <MDBRow>
                   <MDBCol sm="1">
-                    <Button>Edit</Button>
+                    <Button onClick={() => setEditMode(true)}>Edit</Button>
                   </MDBCol>
                   <MDBCol sm="1">
-                    <Button>Save</Button>
+                    <Button onClick={handleToEdit}>Save</Button>
                   </MDBCol>
                   <MDBCol sm="1">
                     <Button
@@ -121,7 +170,6 @@ export default function Profile() {
           </MDBContainer>
         </section>
       ) : null}
-      <Footer />
     </>
   );
 }
