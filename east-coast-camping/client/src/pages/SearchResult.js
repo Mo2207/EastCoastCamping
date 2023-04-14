f46e52d31d60e465bbd0a94a8cb8c52dd4c37a52
Aung Han <doubleaung5@gmail.com>
2023-04-14 17:30:08 -0300

    add view details btn

--- east-coast-camping/client/src/pages/SearchResult.js
+++ east-coast-camping/client/src/pages/SearchResult.js
@@ -5,12 +5,14 @@ import Row from 'react-bootstrap/Row';
 import { Container } from 'react-bootstrap';
 import 'react-datepicker/dist/react-datepicker.css'
 import { Link, useLocation } from 'react-router-dom';
-import { QUERY_CAMPGROUNDBYLOCATION } from '../utils/queries';
-import { useLazyQuery } from '@apollo/client';
+import { QUERY_CAMPGROUNDBYLOCATION, QUERY_ALLCAMPS } from '../utils/queries';
+import { useLazyQuery, useQuery } from '@apollo/client';
 
 function SearchResult() {
     const [destination, setDestination] = useState('');
     const [searchCampgrounds, { loading, error, data }] = useLazyQuery(QUERY_CAMPGROUNDBYLOCATION);
+    const allCampsData = useQuery(QUERY_ALLCAMPS);
+    console.log(allCampsData)
     const location = useLocation();
 
     const handleSubmit = async (event) => {
@@ -26,7 +28,6 @@ function SearchResult() {
 
     // Retrieve destination value from URL query parameter on initial render
     // [] will run only once
-
     React.useEffect(() => {
         // create new object from 'location.search' that contain param from URL
         const searchParams = new URLSearchParams(location.search);
@@ -51,7 +52,6 @@ function SearchResult() {
                 <Card.Body className='d-flex align-items-center searchBar '>
                     <form onSubmit={handleSubmit}>
                         <select value={destination} onChange={(e) => setDestination(e.target.value)}>
-                            <option value="">Search destinations</option>
                             <option value="Fredericton">Fredericton</option>
                             <option value="Saint John">Saint John</option>
                             <option value="Moncton">Moncton</option>
@@ -71,22 +71,22 @@ function SearchResult() {
             </Card>
 
             <Container>
-                <Row>
-                    <Col>
-                        <Row xs={1} md={2} className="g-4">
+                <Row >
+                    <Col >
+                        <Row xs={1} md={2} className="g-4 ">
                             {data &&
                                 data.campByLocation &&
                                 data.campByLocation.map((campground, idx) => (
-                                    <Col key={idx}>
-                                        <Card>
+                                    <Col className='p-4' key={idx}>
+                                        <Card className='mb-5'>
                                             {/* Pass the campground information as query parameters in the URL */}
-                                            <Link to={`/campground/${campground.id}?name=${campground.name}&location=${campground.location}&price=${campground.price}`}>
-                                                View Details
-                                                <Card.Img
-                                                    variant="top"
-                                                    src="https://user-images.githubusercontent.com/112873819/231262296-5bbbe70c-886e-4501-ab8c-df9403029aa3.jpg"
-                                                />
-                                            </Link>
+
+                                            <Card.Img
+                                                variant="top"
+                                                src="https://user-images.githubusercontent.com/112873819/231262296-5bbbe70c-886e-4501-ab8c-df9403029aa3.jpg"
+                                            />
+                                            <div> View Details</div>
+
                                             <Card.Body>
                                                 <Card.Title>{campground.name}</Card.Title>
                                                 <Card.Text>
@@ -94,6 +94,12 @@ function SearchResult() {
                                                     Price: {campground.price}
                                                 </Card.Text>
                                             </Card.Body>
+                                            <div className='d-flex justify-content-end btn'>
+                                                <Link to={`/campground/${campground.id}?name=${campground.name}&location=${campground.location}&price=${campground.price}`}>
+                                                    <button className='btn' style={{ backgroundColor: '#ADFB2F' }}>View Details</button>
+                                                </Link>
+                                            </div>
+
                                         </Card>
                                     </Col>
                                 ))}
