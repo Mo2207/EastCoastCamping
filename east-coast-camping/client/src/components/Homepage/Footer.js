d4331413c95ebdf9cb9d981fcbd9d2f065a4c5d8
Aung Han <doubleaung5@gmail.com>
2023-04-14 09:17:45 -0300

    updated footer padding

--- east-coast-camping/client/src/components/Homepage/Footer.js
+++ east-coast-camping/client/src/components/Homepage/Footer.js
@@ -7,10 +7,10 @@ import React from 'react';
 export default function Footer() {
     return (
 
-        <footer style={{ backgroundColor: 'black' }}>
+        <footer style={{ backgroundColor: 'black', maxHeight: '200px' }}>
             <h3 className='text-white text-center pt-5'>Life is better around the campfire</h3>
 
-            <div className='text-white text-center py-5'>
+            <div className='text-white text-center py-4'>
                 <small >&copy; 2023 EastCoastCamping. All rights reserved.</small>
             </div>
 
