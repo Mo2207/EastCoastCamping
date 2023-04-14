ccf819d92fe7a175df657b8c97bb8f619d2d79ff
Gabe Morris <gabemorris1042@gmail.com>
2023-04-14 11:20:54 -0300

    changed variable name to just userId

--- east-coast-camping/client/src/utils/queries.js
+++ east-coast-camping/client/src/utils/queries.js
@@ -15,8 +15,8 @@ query AllCamps {
 
 
 export const QUERY_ME = gql`
-  query Query($userByIdId: ID!) {
-    userById(id: $userByIdId) {
+  query Query($userId: ID!) {
+    userById(id: $userId) {
       email
       firstName
       lastName
