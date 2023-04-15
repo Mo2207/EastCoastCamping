6d56a1959cd99ac71638dc42486f4ab9de7695ff
Gabe Morris <gabemorris1042@gmail.com>
2023-04-15 15:19:49 -0300

    added Review component and Review.css

---
+++ east-coast-camping/client/src/components/Review.js
@@ -0,0 +1,22 @@
+
+import React from "react";
+import { 
+    Container,
+    Row,
+    Col,
+    Card,
+    Image,
+    Button 
+  } from 'react-bootstrap';
+import "../styles/Review.css";
+
+export default function Review() {
+
+  return (
+    <>
+      <Container fluid>
+        Leave a review
+      </Container>    
+    </>
+  );
+}
