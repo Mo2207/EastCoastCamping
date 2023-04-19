b6401fe235b6d48ae415e9499c756ae4de8cd630
Yelzaw <110345251+Yelzaw@users.noreply.github.com>
2023-04-19 11:22:48 -0300

    Updating CSS

--- east-coast-camping/client/src/pages/SearchResult.js
+++ east-coast-camping/client/src/pages/SearchResult.js
@@ -132,35 +132,7 @@ function SearchResult() {
                                                 </Row>
                                             </Card>
                                         </Col>
-                                        className="row d-flex flex-wrap justify-content-center mb-3"
-                                        style={{ marginRight: '15px' }}>
-                                               
-                                        <Card className="card" style={{ minWidth: '19rem', minHeight: '450px', margin: '20px' }}>
-                                          <Card.Img
-                                            variant="top"
-                                            src={campground.image}
-                                            alt={campground.name}
-                                            style={{ marginTop: '10px' }}
-                                          />
-                                          <Card.Body>
-                                            <Card.Title>{campground.name}</Card.Title>
-                                            <Card.Text>
-                                              <strong>Location: </strong>{campground.location}<br />
-                                              <strong>Price: </strong>{campground.price}<br/>
-                                              <strong>Rating: </strong>{Start(campground.reviews[0].rating)}<br />
-                                              <strong>Review: </strong>{campground.reviews[0].text}<br />
-                                            </Card.Text>
-                                            </Card.Body>
-                                            <Row className="d-flex justify-content-between">
-                                                <Col className="btn">
-                                                <Button href={`/review/${campground._id}`} variant="primary" size="sm">Review</Button>
-                                                </Col>
-                                                <Col className="btn">
-                                                <Button href={`/campground/${campground._id}?name=${campground.name}`} variant="primary" size="sm">View Details</Button>
-                                                </Col>
-                                            </Row>
-                                        </Card>
-                                      </Col>   
+                                        
                                     ))}
                             </Row>
                         </Col>
