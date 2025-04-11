ee9a20fbbba616844d05a2e301653b9abf0246f5
Gabe Morris <gabemorris1042@gmail.com>
2025-04-11 11:43:24 -0300

    remove nested duplicate project folder

--- east-coast-camping/server/models/index.js
+++
@@ -1,6 +0,0 @@
-
-const User = require('./User');
-const CampGround = require('./Campground');
-const Review = require('./Review');
-const Booking = require(`./Bookings`);
-module.exports = { User, CampGround, Review, Booking};
